6afd4d5e31ea9ca0a552da4c1b5591618e751385
Hiroyugane <Hiroyugane@gmail.com>
2021-01-15 21:36:38 +0100

    reworking __init__

--- motp/config/__init__.py
+++ motp/config/__init__.py
@@ -1,4 +1,42 @@
-# Debug Info
-from pathlib import Path
-print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+#!/usr/bin/env python3
+# Status: New, not tested yet, not published
+######################################################
+# Info-Documentation
+######################################################
+# Input: -
+#         
+# Output-Example: 
+# {
+# }
 #
+# To-Do, Ideas:
+#      
+######################################################
+# Foundation, do not change
+######################################################
+import logging
+from pathlib import Path
+import inspect
+
+from ..baselib import base
+
+logging.debug('Running '+str(Path(__file__).resolve()) if __name__ == '__main__' else 'Importing '+str(Path(__file__).resolve()))
+def whoami():
+    return inspect.stack()[1][3]
+def whosparent():
+    return inspect.stack()[2][3]
+######################################################
+# Imports
+######################################################
+######################################################
+# In-File Config
+######################################################
+######################################################
+# Functions
+######################################################
+######################################################
+# Default clause
+######################################################
+logging.debug('Finished Running '+str(Path(__file__).resolve()) if __name__ == '__main__' else 'Finished Importing '+str(Path(__file__).resolve()))
+#EOF
+
