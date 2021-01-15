2a3cb9837aae6337d9de082f86874198f1b5e567
Hiroyugane <Hiroyugane@gmail.com>
2021-01-16 00:17:01 +0100

    reworking base.py (wip)

--- motp/baselib/base.py
+++ motp/baselib/base.py
@@ -57,26 +57,77 @@ except Exception:
         logging.warn("copied config_main.ini.bak to config_main.ini because python couldn't find a valid config")
 else:
     logging.debug("read config_main.ini successfully")
+# format color dictionary
+colors_raw = config._sections['colors']
+colors = {}
+for color in colors_raw:
+    colors[color] = chr(0x1b) + colors_raw[color]
 ######################################################
 # Functions, Methods (Code)
 ######################################################
 # run commands as if they'd be used shell
-def templatefunction():
-	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
-	try:
-		#some stuff
-		print()
-	except: 
-		logging.critical("Critical Error message")
-		return
-	else:
-		logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
-		return
+def colored(col:str, s:str):
+    logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    try:
+        colored_result = colors[col] + s + colors['reset']
+    except: 
+        logging.critical("Critical Error message")
+        return
+    else:
+        logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+        return colored_result
 
+# takes int of bytes and puts them into the right unit (/1024). Keeps values mostly to 3 digits
+def humanise_byte(int_:int):
+    logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    try:
+        for x in ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']:
+            if int_ < 1024.0:
+                if int_ < 1:
+                    int_return = "%1.3f %s" % (int_, x)
+                    break
+                elif int_ < 10:
+                    int_return = "%1.2f %s" % (int_, x)
+                    break
+                elif int_ < 100:
+                    int_return = "%1.1f %s" % (int_, x)
+                    break
+                else:
+                    int_return = "%0.0f %s" % (int_, x)
+                    break
+            else: int_ /= 1024.00
+    except Exception:
+        logging.critical("Critical Error with humanising: "+int_)
+        return
+    else:
+        logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+        return int_return
 
-
-
-
+# takes int of bits and puts them into the right unit (/1000). Keeps values mostly to 3 digits
+def humanise_bit(int_:int):
+    logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    try:
+        for x in ['bit', 'Kbit', 'Mbit', 'Gbit', 'Tbit', 'Pbit', 'Ebit', 'Zbit', 'Ybit']:
+            if int_ < 1000.0:
+                if int_ < 1:
+                    int_return = "%1.3f %s" % (int_, x)
+                    break
+                elif int_ < 10:
+                    int_return = "%1.2f %s" % (int_, x)
+                    break
+                elif int_ < 100:
+                    int_return = "%1.1f %s" % (int_, x)
+                    break
+                else:
+                    int_return = "%0.0f %s" % (int_, x)
+                    break
+            else: int_ /= 1000.00
+    except Exception:
+        logging.critical("Critical Error with humanising: "+int_)
+        return
+    else:
+        logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+        return int_return
 
 
 
@@ -114,28 +165,28 @@ def templatefunction():
 
 # description of function
 def templatefunction():
-	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
-	try:
-		#some stuff
-		print()
-	except: 
-		logging.critical("Critical Error message")
-		return
-	else:
-		logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
-		return
+    logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    try:
+        #some stuff
+        print()
+    except: 
+        logging.critical("Critical Error message")
+        return
+    else:
+        logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+        return
 ######################################################
 # Main
 ######################################################
 def main():
-	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
-	print("this is a template, if it is main, it will print out the output of all functions for testing purposes")
-	logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+    print("this is a template, if it is main, it will print out the output of all functions for testing purposes")
+    logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
 ######################################################
 # Default clause
 ######################################################
 if __name__ == "__main__":
-	main()
+    main()
 
 logging.debug('Finished Running '+str(Path(__file__).resolve()) if __name__ == '__main__' else 'Finished Importing '+str(Path(__file__).resolve()))
 #EOF
@@ -187,40 +238,14 @@ logging.debug('Finished Running '+str(Path(__file__).resolve()) if __name__ == '
 
 
 
-# format color dictionary
-COLORS_raw = config._sections['colors']
-COLORS = {}
-for color in COLORS_raw:
-    COLORS[color] = chr(0x1b) + COLORS_raw[color]
-
-
-##; Functions
-
-# colorize a string and reset color afterwards 
-def colored(col, s):
-    return COLORS[col] + s + COLORS['reset']
-
-
-# divide bytes by 1024 and change suffix to Datasize "1GB is easier than 1024MB etc"
-def humanise(byte_int):
-    for x in ['bytes', 'KiB', 'MiB', 'GiB', 'TiB']:
-        #if bigger than 1024, divide, go a size further, till under 1024
-        if byte_int < 1024.0:
-            #have a minimum of 3-digit-numbers incl. decimals
-            if byte_int < 10:
-                return "%1.2f%s" % (byte_int, x)
-            else:
-                return "%4.1f%s" % (byte_int, x)
-        byte_int /= 1024.00
-
 
 # smart length: check the length of the line as it'd be output
 def smartlen(line):
     # replace tabs with 4 whitespaces
     line = line.replace("\t", ' '*4)
     #remove colorcodes
-    for color in COLORS.keys():
-        line = line.replace(COLORS[color], '')
+    for color in colors.keys():
+        line = line.replace(colors[color], '')
     return len(line)
 
 # is used to use commands easier. BASE DEF
@@ -230,7 +255,7 @@ def run_cmd(cmd):
 # Functiontests
 def color_test():
     colortest_result = []
-    for color in COLORS:
+    for color in colors:
         colortest_message = colored(color, color)
         colortest_result.append(print(colortest_message))
     return colortest_result
