d0ac1e511829f75e644235fb01454e9ab8bceb28
Hiroyugane <Hiroyugane@gmail.com>
2021-01-15 23:07:15 +0100

    reworking base.py

--- motp/baselib/base.py
+++ motp/baselib/base.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python3
 # Base Module which is being used in every other python module
 # Status: being reworked
-# Debug Info
-from pathlib import Path
-print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+######################################################
+# Info-Documentation
+######################################################
+# Input: -
+#         
+# Output-Example: 
+# {
+# }
 #
+# To-Do, Ideas:
+#      
+######################################################
+# Foundation, do not change
+######################################################
+import logging
+from pathlib import Path
+import inspect
+from ..baselib import base
+
+logging.debug('Running '+str(Path(__file__).resolve()) if __name__ == '__main__' else 'Importing '+str(Path(__file__).resolve()))
+def whoami():
+    return inspect.stack()[1][3]
+def whosparent():
+    return inspect.stack()[2][3]
+######################################################
+# Imports
+######################################################
 import math
 import subprocess
 import os
@@ -15,15 +39,152 @@ from configparser import ConfigParser
 import calendar
 import datetime
 import sys
-
-#import threshold_color
-
-
-##; Configwork
-
-# read config
+import shutil
+######################################################
+# In-File Config, prework
+######################################################
+#Read Config
 config = ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), '..', 'config', 'config_main.ini'))
+configpath = os.path.join(os.path.dirname(__file__), '..', 'config')
+try:
+    config.read(os.path.join(os.path.dirname(__file__), '..', 'config', 'config_main.ini'))
+except Exception:
+    try:
+        shutil.copyfile(os.path.join(configpath, 'config_main.ini.back'), os.path.join(configpath, 'config_main.ini'))
+    except Exception:
+        logging.critical("config_main.ini not found and backup couldn't be restored, aborting")
+    else:
+        logging.warn("copied config_main.ini.bak to config_main.ini because python couldn't find a valid config")
+else:
+    logging.debug("read config_main.ini successfully")
+######################################################
+# Functions, Methods (Code)
+######################################################
+# run commands as if they'd be used shell
+def templatefunction():
+	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+	try:
+		#some stuff
+		print()
+	except: 
+		logging.critical("Critical Error message")
+		return
+	else:
+		logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+		return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# description of function
+def templatefunction():
+	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+	try:
+		#some stuff
+		print()
+	except: 
+		logging.critical("Critical Error message")
+		return
+	else:
+		logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+		return
+######################################################
+# Main
+######################################################
+def main():
+	logging.debug("Executing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+	print("this is a template, if it is main, it will print out the output of all functions for testing purposes")
+	logging.debug("Finishing "+str(Path(__file__).resolve())+": "+whoami()+" ("+whosparent()+")")
+######################################################
+# Default clause
+######################################################
+if __name__ == "__main__":
+	main()
+
+logging.debug('Finished Running '+str(Path(__file__).resolve()) if __name__ == '__main__' else 'Finished Importing '+str(Path(__file__).resolve()))
+#EOF
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # format color dictionary
